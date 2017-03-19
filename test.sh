6a2d92bce07addfd48184df30f0078a1b0562358
r2h2 <rainer@hoerbe.at>
2017-03-19 13:36:13 +0100

    added get_metadata unit test

--- test.sh
+++ test.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
+set -e -o pipefail
 
-dscripts/build.sh
-dscripts/push.sh
-dscripts/sign.sh
-dscripts/verify.sh
-dscripts/run.sh
+main () {
+    source ./conf.sh
+    dscripts/build.sh
+    dscripts/push.sh
+    test_get_metadata
+    dscripts/sign.sh
+    dscripts/verify.sh
+    dscripts/run.sh -ip echo OK
+}
+
+
+test_get_metadata() {
+    key='capabilities'
+    expected_value='--cap-drop=all'
+    value=$(dscripts/get_metadata.py $IMAGENAME $key)
+    if [ "$value" == "$expected_value" ]; then
+        echo "test passed"
+    else
+        echo "test failed: expected $expected_value, but found $value."
+    fi
+}
+
+
+main
